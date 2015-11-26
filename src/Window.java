1ab1bfcb522dbcc80c37ba731093eb2ea27d6a38
Gordie Chown <gordie.chown@gmail.com>
2015-11-26 11:17:24 -0500

    Made font work

--- src/Window.java
+++ src/Window.java
@@ -1,16 +1,17 @@
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.SpringLayout;
-import javax.swing.JPasswordField;
-import javax.swing.JTable;
-import javax.swing.JEditorPane;
-import javax.swing.JScrollPane;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import java.awt.Font;
 import java.awt.GraphicsEnvironment;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComboBox;
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SpringLayout;
+import javax.swing.text.Document;
+import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.StyleSheet;
 
 public class Window {
 
@@ -18,9 +19,13 @@ public class Window {
 	private JEditorPane editor;
 	private SpringLayout springLayout;
 	private JComboBox fontSelector;
-	private	JScrollPane editorScrollPane;
+	private JScrollPane editorScrollPane;
+	private String text;
 	private String font;
-	private int size;
+	private String colour;
+	private HTMLEditorKit kit;
+	private StyleSheet styleSheet;
+
 	/**
 	 * Don't worry about this stuff
 	 */
@@ -48,43 +53,61 @@ public class Window {
 	 * Put your code in here!
 	 */
 	private void initialize() {
-        String color = "purple";
-	String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
-	fontSelector = new JComboBox(fonts);
-	fontSelector.setSelectedIndex(5);
-	font = (String)fontSelector.getSelectedItem();
-        String css = "<style> p { color: " + color + "; font-family: \"" + font + "\";}</style>";
-	System.out.println("CSS: " + css);
-	String text = "Hello, this is a test";
-	frame = new JFrame();
-        editor = new JEditorPane("text/html", css + "<p>" + text + "</p>");
-	editor.setEditable(true);
-        editorScrollPane = new JScrollPane(editor);
-	frame.setBounds(100, 100, 450, 300);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setTitle("Stylesheet editor");
-	//We're using SpringLayout - edit from the Design tab
-	 springLayout = new SpringLayout();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        editorScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-	fontSelector.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent evt) {
-			String css = "<style> p { color: " + color + "; font-family: \"" + fontSelector.getSelectedItem() + "\";}</style>";
-			System.out.println("CSS is " + css);
-					     }});
-	layout();
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Stylesheet editor");
+		frame.setLocationRelativeTo(null);
+		frame.setBounds(100, 100, 450, 300);
+		// We're using SpringLayout - only add elements here, making it look
+		// good happens in layout().
+		springLayout = new SpringLayout();
+		colour = "blue";
+		kit = new HTMLEditorKit();
+		styleSheet = kit.getStyleSheet();
+		//TODO: fix scrolling, make everything not go off the side of the screen!
+		text = "<p>Hello there I'm going to try to make this as long as possible so it goes off the edge"
+				+ "of the wrong thing! hashasoifuhqwieufhjiqwuehjfiquhj</p>";
+		String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+		fontSelector = new JComboBox(fonts);
+		fontSelector.setSelectedIndex(5); font = (String) fontSelector.getSelectedItem();
+		styleSheet.addRule("p {color:" + colour + "; font-family:\"" + font + "\";}");
+		System.out.println("p {color:" + colour + "; font-family:\"" + font + "\";}");
+		editor = new JEditorPane();
+		editor.setEditable(true);
+		editor.setEditorKit(kit);
+		editor.setText(text);
+
+		editorScrollPane = new JScrollPane(editor);
+		fontSelector.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				font = (String) fontSelector.getSelectedItem();
+				styleSheet.addRule("p {color:" + colour + "; font-family:\"" + font + "\";}");
+				updateCSS(styleSheet.toString(), editor.getText());
+			}
+		});
+		layout();
 	}
 
-	private void updateCSS(String css, String text){
-
-	}
 	/**
-	 * Updates the layout
-	 * */
-	private void layout(){
-	frame.getContentPane().setLayout(springLayout);
-        frame.getContentPane().add(editor);
-	frame.getContentPane().add(fontSelector);
-	springLayout.putConstraint(SpringLayout.NORTH, editor, 5, SpringLayout.SOUTH, fontSelector);
+	 * Updates CSS with values.
+	 * 
+	 * @param css
+	 * @param text
+	 */
+	private void updateCSS(String css, String text) {
+		styleSheet.addRule(css);
+		editor.setText(text);
+	}
+
+	/**
+	 * Makes things look good in the layout- anything not related to functionality.
+	 */
+	private void layout() {
+		frame.getContentPane().setLayout(springLayout);
+		frame.getContentPane().add(editorScrollPane);
+		editorScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        editorScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		frame.getContentPane().add(fontSelector);
+		springLayout.putConstraint(SpringLayout.NORTH, editorScrollPane, 5, SpringLayout.SOUTH, fontSelector);
 	}
 }
